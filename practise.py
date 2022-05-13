7b03480a80554d18ab9d23be963362cf7c611152
DenisWAD <denismurray2@gmail.com>
2022-05-13 15:42:00 +0100

    Break and Continue statements

--- practise.py
+++ practise.py
@@ -1,15 +1,7 @@
-my_tuple = (1, 2, 3, 4, 5)
+for i in range (100, 200, 2) :
+  print("i is ", i)
+  if i == 120 :
+    print("i is equivalent to 120")
+    break
+  
 
-for i in my_tuple :
-  print(i)
-
-  cars = {
-    "range" : 2000,
-    "price" : 30000.00,
-    "model" : "Renault Clio",
-    "doors" : 4
-
-  }
-
-  for z in cars.values() :
-    print(z)
