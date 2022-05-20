5941f1b7a9a10491c1b10cfd9b368c65f90ba32d
DenisWAD <denismurray2@gmail.com>
2022-05-20 09:33:30 +0100

    Interim assessment 1 prep

--- practise.py
+++ practise.py
@@ -1,19 +1,27 @@
-import random
+# import random
 
 
-random_list = []
+# random_list = []
 
-# for i in range(10) :
-#   random_list.append(random.randint(0, 100))
+# # for i in range(10) :
+# #   random_list.append(random.randint(0, 100))
 
-[random_list.append(random.randint(0, 100)) for j in range(10) ]
+# [random_list.append(random.randint(0, 100)) for j in range(10) ]
 
-for i in random_list :
-  if i % 7 == 0 :
-    continue
-  elif i == 315 :
-    break
+# for i in random_list :
+#   if i % 7 == 0 :
+#     continue
+#   elif i == 315 :
+#     break
 
-  print(i)
+#   print(i)
 
-# print(random_list)
+# # print(random_list)
+
+i = 0
+
+
+eg = [1, 2, 3]
+for egs in eg :
+  print(eg[i])
+  i += 1
--- whileLoops.py
+++ whileLoops.py
@@ -1,18 +1,41 @@
-# num_digits = 0 
+# Sample Question Mr.Bunty's Wild Ride
+max_capacity = 5
+current_capacity = 0
+eligible = True
+age_card = ""
+total_rides = 0
 
-# int_value = int(input("Input an integer number:"))
+age = 0
 
+while eligible == True :
+  current_capacity = int(input("How many people are already on the ride?"))
 
-# while  int_value > 0 :
-#   int_value //= 10
-#   num_digits += 1
+  # Check space on ride
+  if current_capacity > max_capacity :
+    print("Sorry, the ride is full!")
+    eligible = False
+    break
 
-# print("Number of digits: ", num_digits)
-
-num_digits = 0
-user_input = int(input("Enter an integer:"))
-
-while(user_input > 0): user_input //= 10; num_digits += 1; print("Hi")
-
-print(num_digits)
-
+  # Check age of customer
+  age = int(input("What age are you?"))
+  if age < 12 :
+    print("Sorry, you're too young for this ride")  
+    eligible = False
+  elif age > 17 :
+    print("Enjoy your ride!")
+    eligible = True
+  else :
+    age_card = str(input("Has the customer a valid age card?"))
+    if age_card != "Y" :
+      print("You must have a valid age card for this ride")
+      eligible = False
+    else :
+      total_rides += 1
+      print("Enjoy your ride! As a minor you can only ride the SpookyTrain a maximum of 3 times")
+      print("This is ride number : ", total_rides)
+      if total_rides == 3 :
+        print("You are not allowed to ride anymore due to the risk of nausea")
+      elif total_rides > 3 :
+        print("You cannot ride anymore, max rides already taken")
+        eligible = False
+        break
