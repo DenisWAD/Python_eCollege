5ce92acdb518104c7d38e37f2ad17d616548e427
DenisWAD <denismurray2@gmail.com>
2022-06-24 23:31:11 +0100

    Some more

--- practise.py
+++ practise.py
@@ -1,25 +1,33 @@
-from fileinput import close
+# """ Creating simple classes and Objects. Manipulating the data in the seperate Objects"""
+
+# class User :
+#     """ The basic user for this app. Currently only only contains names, age, basic info. 
+#     But soon will contain a stupid amount of info"""
+#     def __init__(self, full_name, age ) :
+#         name_pieces = full_name.split(" ")
+        
+#         self.first_name = name_pieces[0]
+#         self.last_name = name_pieces[-1]
+#         self.age = age
+
+#     def getAge(self) :
+#         return self.age
+
+#     def getName(self) :
+#         return self.first_name , self.last_name
 
 
-def open_file() :
-    try :
-        f = open("filename1.txt")
-        print( 1 / 0)
-    except FileNotFoundError as err :
-        print("Error: unable to find file\n", repr(err))
-    except ValueError as err:
-        print("Value incorrect: ", repr(err))
-    except ZeroDivisionError as err :
-        print("Cannot divide by zero", repr(err))
-    except IOError as err:
-        print("IOError", repr(err))
-    else :
-        for i in f :
-            print(i.strip("\n"))
-        close()
-    finally :
-        print("Finished program")
+# user1 = User("Denis Murray", 55)
+# print(user1.getName())
 
 
-open_file()
-
+try:
+    n = int(input("How old are you?"))
+    percent = round(n * 100/80, 1)
+    print("You've gone through", percent, "% of your life!")
+except ValueError:
+    print("Oops, must enter a number.")
+except ZeroDivisionError:
+    print("Division by zero")
+except:
+    print("Something went very wrong")
