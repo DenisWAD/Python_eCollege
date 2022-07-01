1077a3f91d2bf6163a51be83685761f83a06533c
DenisWAD <denismurray2@gmail.com>
2022-07-01 14:38:17 +0100

    BAsic practise to module

--- practise.py
+++ practise.py
@@ -1,33 +1,20 @@
-# """ Creating simple classes and Objects. Manipulating the data in the seperate Objects"""
+class User :
+    def __init__(self, name, age, address, mobile) :
+        self.name = name
+        self.age = age
+        self.address = address
+        self.mobile = mobile
 
-# class User :
-#     """ The basic user for this app. Currently only only contains names, age, basic info. 
-#     But soon will contain a stupid amount of info"""
-#     def __init__(self, full_name, age ) :
-#         name_pieces = full_name.split(" ")
+    # def getAge() :
         
-#         self.first_name = name_pieces[0]
-#         self.last_name = name_pieces[-1]
-#         self.age = age
 
-#     def getAge(self) :
-#         return self.age
+user1 = User("Denis Murray", 27, "Kilkenny", 353877838463)
 
-#     def getName(self) :
-#         return self.first_name , self.last_name
+# print(f"Name: {user1.name}\nAge: {user1.age}\nAddress: {user1.address}\nMobile: {user1.mobile}")
+# print(user1.__dict__)
 
 
-# user1 = User("Denis Murray", 55)
-# print(user1.getName())
+def getUserInfo() :
+    return user1.__dict__
 
 
-try:
-    n = int(input("How old are you?"))
-    percent = round(n * 100/80, 1)
-    print("You've gone through", percent, "% of your life!")
-except ValueError:
-    print("Oops, must enter a number.")
-except ZeroDivisionError:
-    print("Division by zero")
-except:
-    print("Something went very wrong")
